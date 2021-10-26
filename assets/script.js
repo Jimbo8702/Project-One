1c2d5df7ed0f8bc776fa2b2dd45909eb31332975
James Sgarella <Jimbo8702@gmail.com>
2021-10-26 10:51:27 -0400

    forgot to save

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+console.log(this);
+
+//DEPENDENCES
+
+//DATA
+
+//FUNCTIONS
+function setUserInput() {}
+//gets the user input and puts it into local storage
+//set field A and field B
+function tripOptions() {}
+//select trip options from an array
+//walk, drive, etc
+function getLocation() {}
+//take user input of city and convert it to zipcode or lat long
+//give api lat long to get informatin back
+
+//USER Interaction
+
+//Inilizations
