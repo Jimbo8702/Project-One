496179514a3a1977885b6a30ba2797921135bbe4
James Sgarella <Jimbo8702@gmail.com>
2021-10-27 13:08:28 -0400

    api is sending back a map

--- assets/script.js
+++ assets/script.js
@@ -8,10 +8,6 @@ var positionStackApiKey = "5e790f29e566f2f69cba98b84c761f7e";
 //DATA
 
 //FUNCTIONS
-function setUserInput() {}
-//gets the user input and puts it into local storage
-//set field A and field B
-//Doing this outside of a function
 function tripOptions() {}
 //select trip options from an array
 //walk, drive, etc
@@ -97,6 +93,87 @@ function getRoute() {
     });
 }
 
+function setRoute() {
+  var locOrigin = JSON.parse(localStorage.getItem("locationA"));
+  var locDestination = JSON.parse(localStorage.getItem("locationB"));
+  var locationOne = locOrigin.latitude + "," + locOrigin.longitude;
+  var locationTwo = locDestination.latitude + "," + locDestination.longitude;
+
+  // Instantiate a map and platform object:
+  var platform = new H.service.Platform({
+    apikey: "owwQPmvcB76JMPSBwfPmXyceEJS1h6eYxi1Sx1bblfI",
+  });
+  // Retrieve the target element for the map:
+  var targetElement = document.getElementById("map-container");
+
+  // Get the default map types from the platform object:
+  var defaultLayers = platform.createDefaultLayers();
+
+  // Instantiate the map:
+  var map = new H.Map(
+    document.getElementById("map-container"),
+    defaultLayers.vector.normal.map,
+    {
+      zoom: 10,
+      center: { lat: 52.51, lng: 13.4 },
+    }
+  );
+
+  // Create the parameters for the routing request:
+  var routingParameters = {
+    routingMode: "fast",
+    transportMode: "car",
+    // The start point of the route:
+    origin: locationOne,
+    // The end point of the route:
+    destination: locationTwo,
+    // Include the route shape in the response
+    return: "polyline",
+  };
+
+  // Define a callback function to process the routing response:
+  var onResult = function (result) {
+    // ensure that at least one route was found
+    if (result.routes.length) {
+      result.routes[0].sections.forEach((section) => {
+        // Create a linestring to use as a point source for the route line
+        let linestring = H.geo.LineString.fromFlexiblePolyline(
+          section.polyline
+        );
+
+        // Create a polyline to display the route:
+        let routeLine = new H.map.Polyline(linestring, {
+          style: { strokeColor: "blue", lineWidth: 3 },
+        });
+
+        // Create a marker for the start point:
+        let startMarker = new H.map.Marker(section.departure.place.location);
+
+        // Create a marker for the end point:
+        let endMarker = new H.map.Marker(section.arrival.place.location);
+
+        // Add the route polyline and the two markers to the map:
+        map.addObjects([routeLine, startMarker, endMarker]);
+
+        // Set the map's viewport to make the whole route visible:
+        map
+          .getViewModel()
+          .setLookAtData({ bounds: routeLine.getBoundingBox() });
+      });
+    }
+  };
+
+  // Get an instance of the routing service version 8:
+  var router = platform.getRoutingService(null, 8);
+
+  // Call calculateRoute() with the routing parameters,
+  // the callback and an error callback function (called if a
+  // communication error occurs):
+  router.calculateRoute(routingParameters, onResult, function (error) {
+    alert(error.message);
+  });
+}
+
 //USER Interaction
 
 //Inilizations
@@ -124,4 +201,5 @@ submit.addEventListener("click", function () {
   getLocationA();
   getLocationB();
   getRoute();
+  setRoute();
 });
